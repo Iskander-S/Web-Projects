2d575bce2445a455361229113ef2ac8cf0f71838
Iskander Sadykov <sady1ov.1skander@gmail.com>
2024-10-13 15:44:31 +0500

    Update index.js

--- Weather App/index.js
+++ Weather App/index.js
@@ -6,7 +6,7 @@ const error404 = document.querySelector('.not-found');
 
 search.addEventListener('click', () => {
 
-    const APIKey = '02810704e2017de42265914c15029d95';
+    const APIKey = 'apikey';
     const city = document.querySelector('.search-box input').value;
 
     if (city === '')
